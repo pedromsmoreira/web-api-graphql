1e535fffe70d642b6ae65ecba839d29f8a3c9175
Pedro Moreira <pedro.ms.moreira51@gmail.com>
2017-07-18 23:42:02 +0100

    new filter

---
+++ src/WebApiGraphCool/WebApi/Attributes/GraphTypeAttribute.cs
@@ -0,0 +1,55 @@
+﻿namespace WebApi.Attributes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net.Http;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using System.Web.Http.Controllers;
+    using System.Web.Http.Filters;
+    using GraphQL;
+    using GraphQL.Types;
+
+    public class GraphTypeAttribute : FilterAttribute, IActionFilter
+    {
+        private readonly string parameterName;
+        private readonly Type parameterType;
+
+        public GraphTypeAttribute(Type type, string parameterName = null)
+        {
+            this.parameterName = parameterName;
+            this.parameterType = type;
+        }
+
+        public async Task<HttpResponseMessage> ExecuteActionFilterAsync(HttpActionContext actionContext, CancellationToken cancellationToken, Func<Task<HttpResponseMessage>> continuation)
+        {
+            string query = Convert.ToString(actionContext.ActionArguments.ToArray().First().Value);
+            var result = await new DocumentExecuter().ExecuteAsync((options) =>
+            {
+                options.Schema = new Schema() { Query = (IObjectGraphType)Activator.CreateInstance(parameterType) };
+                options.Query = query;
+            });
+            actionContext.ActionArguments[parameterName ?? actionContext.ActionArguments.ToArray().First().Key] = result;
+            return await continuation();
+        }
+
+        private void _checkForErrors(ExecutionResult result)
+        {
+            if (result.Errors?.Count > 0)
+            {
+                var errors = new List<Exception>();
+                foreach (var error in result.Errors)
+                {
+                    var ex = new Exception(error.Message);
+                    if (error.InnerException != null)
+                    {
+                        ex = new Exception(error.Message, error.InnerException);
+                    }
+                    errors.Add(ex);
+                }
+                throw new AggregateException(errors);
+            }
+        }
+    }
+}
--- src/WebApiGraphCool/WebApi/Controllers/GraphController.cs
+++ src/WebApiGraphCool/WebApi/Controllers/GraphController.cs
@@ -1,51 +1,23 @@
 ﻿namespace WebApi.Controllers
 {
-    using System;
-    using System.Collections.Generic;
     using System.Threading.Tasks;
     using System.Web.Http;
+    using Attributes;
     using GraphQL;
     using GraphQL.Http;
-    using GraphQL.Types;
     using Newtonsoft.Json.Linq;
     using Queries;
-    using Repositories;
 
     public class GraphController : ApiController
     {
         [HttpPost]
         [Route("~/api/graph")]
-        public async Task<IHttpActionResult> Post([FromBody] string query)
+        [GraphType(typeof(BooksQuery))]
+        public async Task<IHttpActionResult> Post([FromBody] ExecutionResult result)
         {
-            var result = await new DocumentExecuter().ExecuteAsync((options) =>
-            {
-                options.Schema = new Schema { Query = new BooksQuery() };
-                options.Query = query;
-            });
-
-            this.ErrorValidation(result);
-
             var json = new DocumentWriter(indent: true).Write(result);
 
             return this.Ok(JObject.Parse(json));
-        }
-
-        private void ErrorValidation(ExecutionResult result)
-        {
-            if (result.Errors?.Count > 0)
-            {
-                var errors = new List<Exception>();
-                foreach (var error in result.Errors)
-                {
-                    var ex = new Exception(error.Message);
-                    if (error.InnerException != null)
-                    {
-                        ex = new Exception(error.Message, error.InnerException);
-                    }
-                    errors.Add(ex);
-                }
-                throw new AggregateException(errors);
-            }
         }
     }
 }
